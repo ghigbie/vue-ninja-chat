f37c021707659566dfdf271dcda40aafd6644864
ghigbie <gehigbie@gmail.com>
2018-07-31 20:31:22 -0600

    Add chat component and basic template

---
+++ src/components/Chat.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="chat">
+        <h2>Chat</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Chat',
+    data(){
+        return {
+
+        };
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
