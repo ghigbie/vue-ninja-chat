3b2a141a1ad8ac36519fa35aaf398a870ffba460
ghigbie <gehigbie@gmail.com>
2018-08-06 20:34:26 -0600

    Add styles

---
+++ src/components/NewMessage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="new-message">
+        <h2>New Message</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewMessage',
+    data(){
+        return{
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
