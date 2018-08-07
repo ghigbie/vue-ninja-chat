9d938e05e1cf84c06dd08bef88a99024cc9598e3
ghigbie <gehigbie@gmail.com>
2018-08-06 20:21:37 -0600

    Include basic markup

---
+++ src/components/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1 class="not-found">Sorry, but this page does not exist.</h1>
+</template>
+
+<script>
+export default {
+    name: 'NotFound',
+    data(){
+        return{
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
