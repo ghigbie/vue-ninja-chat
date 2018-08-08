8a56726e652e7aef7ed0902dd7a603e9b966272a
ghigbie <gehigbie@gmail.com>
2018-08-07 19:30:28 -0600

    Edit add message fucntion

--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -4,26 +4,36 @@
         <form @submit.prevent="addMessage">
             <label for="new-message">New Message (enter to add)</label>
             <input type="text" name="new-message" />
+            <p class="red-text" v-if="feedback">{{ feedback }}</p>
+            <button></button>
         </form>
 
     </div>
 </template>
 
 <script>
+import db from '@/firebase/init';
+
 export default {
     name: 'NewMessage',
     props: ['name'],
     data(){
         return{
             newMessage: null,
+            feedback: null
         };
     },
     methods:{
         addMessage(){
-            console.log('Add message called');
-            console.log(this.message);
-            console.log(this.name);
-            console.log(Date.now());
+            if(this.newMessage){
+                db.collection('message').add({
+                    content: this.newMessage,
+                    name: this.name,
+                    timestamp: Date.now()
+                })
+            }else{
+                this.feedback = 'Please enter a message to chat : )'
+            }
         }
     }
 }
